b07371e4eb10b59b39cc056d27966a0fe7997cde
Casen Luck <casenluck@gmail.com>
2021-10-02 11:27:35 -0400

    added comments in JS file and updated the readme file

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-
 function writePassword() {
+  // assigns the result from the generate pasword function to password
   var password = generatePassword();
+  // sets passwordText equal to the contents of the html elements with ID #password
   var passwordText = document.querySelector("#password");
-
+  //value of password text is set to equal password
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Event Listerner for when user presses generate button which then runs the writepassword function
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
@@ -25,17 +25,21 @@ function generatePassword(){
   let passLCase = "";
   
   let pass = '';
+  // first iterator set to j
   let j = 0;
-
+  // first prompt loop asking how many characters user wants
   do{
   passLength = parseInt(prompt("How many characters do you want in the password? Enter a number from 8-128"));
+  // checks if answer is between 8 and 128
     if(passLength >= 8 && passLength <= 128){
       j++;
     } else{
+      // displays error message then loops back to reask question
       alert("Error you didn't enter a number between 8-128. Please retry :)");
     }
   }while(j != 1)
 
+  // second prompt loop asking if user wants  numbers
   do{
   passNumbers = prompt("Do you want numbers in your password? Enter Y or N");
     if(passNumbers.toLowerCase() == "y" || passNumbers.toLowerCase() == "n"){
@@ -45,6 +49,7 @@ function generatePassword(){
     }
   }while(j != 2)
   
+  //third prompt loop asking if user wants  special characters
   do{
   passSpecial = prompt("Do you want special characters in your password? Enter Y or N");
   if(passSpecial.toLowerCase() == "y" ||  passSpecial.toLowerCase() == "n"){
@@ -53,7 +58,7 @@ function generatePassword(){
     alert("Error you didn't enter Y or N. Please retry :)")
   }
   }while(j != 3)
-
+  // fourth prompt loop asking if user wants lower case letters
   do{
     passLCase = prompt("Do you want lower case letters in the password? Enter Y or N");
     if(passLCase.toLowerCase() == "y" ||  passLCase.toLowerCase() == "n"){
@@ -62,7 +67,7 @@ function generatePassword(){
       alert("Error you didn't enter Y or N. Please retry :)");
     }
   }while(j != 4)
-
+  // fifth prompt loop asking if user wants  upper case letters
   do{
     passUCase = prompt("Do you want upper case letters in the password? Enter Y or N");
     if(passUCase.toLowerCase() == "y" ||  passUCase.toLowerCase() == "n"){
@@ -73,7 +78,7 @@ function generatePassword(){
   }while(j != 5)
 
   
-
+  // following ifs determine whether user said yes or no to previous questions
   if(passNumbers.toLowerCase() == "y"){
     useNumber = true;
   }
@@ -88,44 +93,45 @@ function generatePassword(){
   }
   
   let i = 0;
+  // password generator loop
   do{
+    //switch loop argument generates a random number between 0 and 3
     switch(Math.floor(Math.random() * 4)){
+      //first case picks a random lower case letter and adds it to the password
       case 0:
         if(useLCase){
           let lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
           pass += lowerLetters.charAt(Math.floor(Math.random()*lowerLetters.length));
-          console.log(pass);
           i++;
       }
       break;
-
+      //second case picks random number and adds it to the password
       case 1:
         if(useNumber){
           let numbers = '1234567890';
           pass += numbers.charAt(Math.floor(Math.random()*numbers.length));
-          console.log(pass);
           i++;
       }
       break;
-
+      //third case picks a random special character and adds it to the pasword
       case 2: 
         if(useSpecial){
-          let special = '#$%&!';
+          let special = '!@#$%^&*()-+={}[];:_<>.,/?|~"`';
           pass += special.charAt(Math.floor(Math.random()*special.length));
-          console.log(pass);
           i++;
         }
       break;
-
+      // fourth case picks a random upper case letter and adds it to the pasword
       case 3:
         if(useUCase){
           let upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
           pass += upperLetters.charAt(Math.floor(Math.random()*upperLetters.length));
-          console.log(pass);
           i++;
         }
         break;
     }
+    //checks if the loop has to loop again or stop once the password length has been reached
   }while(i < passLength)
+  //returns the generated password
   return(pass);
 }
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Homework3
+# Password Generator
+
+This projects title is Password Generator and it is a website that I coded the javascript for using my new found javascript coding skills so that it follows acceptance criteria. 
+
+## Languages
+
+Used javascript to code the backend of this website.
+
+## Objective
+
+- Make it so when the generate password button is clicked a password a series of prompts appears
+- When prompts appear, user is able to select which criteria they want including numnbers, special character, and upper/lowercase letters
+- When prompted for password length the user can only select a password betwen 8 and 128 characters
+- When user answers each prompt the answer is validates otherwise it re asks the question
+- When all prompts are answered then a password is generated for the user on the page
+
+
+## What was learned
+
+I learned a lot more about how to use eventlisteners as well as various loops in javascript such as do while loops and switch case.
+
+## Project pictures/websitelink 
+<!-- MODIFY BELOW LINKS BEFORE SUBMITTING !!!!!!!!!!!!!!!!!!!!!!!!! -->
+Screenshots below!
+![Image of website](./Assets/03-javascript-homework-demo.png)
+
+
+Link to Password Generator
+[Password Generator](https://cluck135.github.io/Password-Generator-Project//)
+
